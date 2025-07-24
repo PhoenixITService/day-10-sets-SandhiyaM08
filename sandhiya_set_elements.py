5a5985c2699ec956c36aa9ac7f78e264856bf37c
SandhiyaM08 <Sandhiya.Muthumani@Coforge.com>
2025-07-24 17:34:09 +0530

    updated

--- sandhiya_set_elements.py
+++ sandhiya_set_elements.py
@@ -2,4 +2,4 @@ A = {1, 2, 3, 4, 5}
 B = {3, 4, 5, 6, 7}
 C = {5, 6, 7, 8, 9}
 D = {1, 3, 7, 9, 10}
-print("Elements in exactly 2 sets: ",((A & B) - (C | D)) |((A & C) - (B | D)) | ((A & D) - (B | C)) |((B & C) - (A | D)) |((B & D) - (A | C)) |((C & D) - (A | B)))
+print("Elements in exactly 2 sets:",((A & B) - (C | D)) |((A & C) - (B | D)) | ((A & D) - (B | C)) |((B & C) - (A | D)) |((B & D) - (A | C)) |((C & D) - (A | B)))
